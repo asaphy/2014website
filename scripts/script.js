ad6fd2f6e7a7b20d7506956ea5be1e07b15737a0
Asaph Yuan <asaphy@bu.edu>
2014-07-29 01:09:35 -0700

    hide nav bar on homepage

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 	$(document).ready(function(){
+/*
 		var J = jQuery.noConflict();
 		J(".block").hover(function(){
 		   J(this).addClass("hover");
@@ -9,6 +10,7 @@
 		         J(self).removeClass('hover');
 		  },500);
 		});
+*/
 		var K = jQuery.noConflict();
 		K('#hi').addClass('animated bounceInLeft');
 		K('#Asaph').addClass('animated flipInX');
@@ -21,25 +23,106 @@
 		K('#God').addClass('animated zoomInDown');
 	});
 	
-	function checkAbout(){ 
-		var K = jQuery.noConflict();
-		K('#st-panel-2').attr('checked',true);
-		var L = jQuery.noConflict();
-		L('.st-scroll').css({"-webkit-transform": "translateY(-100%)"});
-	}
-	
+			
+		
 	function checkHome(){ 
 		var K = jQuery.noConflict();
 		K('#st-panel-1').attr('checked',true);
 		var L = jQuery.noConflict();
 		L('.st-scroll').css({"-webkit-transform": "translateY(0%)"});
+		var M = jQuery.noConflict();
+		M('#st-control-1').css("left", "-100%");
+		M('#st-control-1link').css("left", "-100%");
+		
+		M('#st-control-2').css("left", "-114%");
+		M('#st-control-2link').css("left", "-114%");
+	
+		M('#st-control-3').css("left", "-128%");
+		M('#st-control-3link').css("left", "-128%");
+		
+		M('#st-control-4').css("left", "-142%");
+		M('#st-control-4link').css("left", "-142%");
+		
+		M('#st-control-5').css("left", "-156%");
+		M('#st-control-5link').css("left", "-156%");
+		
+		M('#st-control-6').css("left", "-170%");
+		M('#st-control-6link').css("left", "-170%");
+		
+		M('#st-control-7').css("left", "-185%");
+		M('#st-control-7link').css("left", "-185%");
+	}	
+	
+	
+	function checkAbout(){ 
+		var K = jQuery.noConflict();
+		K('#st-panel-2').attr('checked',true);
+		var L = jQuery.noConflict();
+		L('.st-scroll').css({"-webkit-transform": "translateY(-100%)"});
+		
+		var M = jQuery.noConflict();
+		M('#st-control-1').css("left", "0%");
+		M('#st-control-1link').css("left", "0%");
+		
+		M('#st-control-2').css("left", "14%");
+		M('#st-control-2link').css("left", "14%");
+		M('#st-control-2link').css("color", "#4FD5D6");
+	
+		M('#st-control-3').css("left", "28%");
+		M('#st-control-3link').css("left", "28%");
+		M('#st-control-3link').css("color", "#FFF");
+		
+		M('#st-control-4').css("left", "42%");
+		M('#st-control-4link').css("left", "42%");
+		M('#st-control-4link').css("color", "#FFF");
+		
+		M('#st-control-5').css("left", "56%");
+		M('#st-control-5link').css("left", "56%");
+		M('#st-control-5link').css("color", "#FFF");
+		
+		M('#st-control-6').css("left", "70%");
+		M('#st-control-6link').css("left", "70%");
+		M('#st-control-6link').css("color", "#FFF");
+		
+		M('#st-control-7').css("left", "85%");
+		M('#st-control-7link').css("left", "85%");
+		M('#st-control-7link').css("color", "#FFF");
 	}
+
 	
 	function checkProjects(){ 
 		var K = jQuery.noConflict();
 		K('#st-panel-1').attr('checked',true);
 		var L = jQuery.noConflict();
 		L('.st-scroll').css({"-webkit-transform": "translateY(-200%)"});
+		
+		var M = jQuery.noConflict();
+		M('#st-control-1').css("left", "0%");
+		M('#st-control-1link').css("left", "0%");
+		
+		M('#st-control-2').css("left", "14%");
+		M('#st-control-2link').css("left", "14%");
+		M('#st-control-2link').css("color", "#FFF");
+	
+		M('#st-control-3').css("left", "28%");
+		M('#st-control-3link').css("left", "28%");
+		M('#st-control-3link').css("color", "#4FD5D6");
+		
+		M('#st-control-4').css("left", "42%");
+		M('#st-control-4link').css("left", "42%");
+		M('#st-control-4link').css("color", "#FFF");
+		
+		M('#st-control-5').css("left", "56%");
+		M('#st-control-5link').css("left", "56%");
+		M('#st-control-5link').css("color", "#FFF");
+		
+		M('#st-control-6').css("left", "70%");
+		M('#st-control-6link').css("left", "70%");
+		M('#st-control-6link').css("color", "#FFF");
+		
+		M('#st-control-7').css("left", "85%");
+		M('#st-control-7link').css("left", "85%");
+		M('#st-control-7link').css("color", "#FFF");
 	}
 	
 	function checkBlog(){ 
@@ -47,6 +130,34 @@
 		K('#st-panel-1').attr('checked',true);
 		var L = jQuery.noConflict();
 		L('.st-scroll').css({"-webkit-transform": "translateY(-300%)"});
+		
+		var M = jQuery.noConflict();
+		M('#st-control-1').css("left", "0%");
+		M('#st-control-1link').css("left", "0%");
+		
+		M('#st-control-2').css("left", "14%");
+		M('#st-control-2link').css("left", "14%");
+		M('#st-control-2link').css("color", "#FFF");
+	
+		M('#st-control-3').css("left", "28%");
+		M('#st-control-3link').css("left", "28%");
+		M('#st-control-3link').css("color", "#FFF");
+		
+		M('#st-control-4').css("left", "42%");
+		M('#st-control-4link').css("left", "42%");
+		M('#st-control-4link').css("color", "#4FD5D6");
+		
+		M('#st-control-5').css("left", "56%");
+		M('#st-control-5link').css("left", "56%");
+		M('#st-control-5link').css("color", "#FFF");
+		
+		M('#st-control-6').css("left", "70%");
+		M('#st-control-6link').css("left", "70%");
+		M('#st-control-6link').css("color", "#FFF");
+		
+		M('#st-control-7').css("left", "85%");
+		M('#st-control-7link').css("left", "85%");
+		M('#st-control-7link').css("color", "#FFF");
 	}
 	
 	function checkMusic(){ 
@@ -54,6 +165,34 @@
 		K('#st-panel-1').attr('checked',true);
 		var L = jQuery.noConflict();
 		L('.st-scroll').css({"-webkit-transform": "translateY(-400%)"});
+		
+		var M = jQuery.noConflict();
+		M('#st-control-1').css("left", "0%");
+		M('#st-control-1link').css("left", "0%");
+		
+		M('#st-control-2').css("left", "14%");
+		M('#st-control-2link').css("left", "14%");
+		M('#st-control-2link').css("color", "#FFF");
+	
+		M('#st-control-3').css("left", "28%");
+		M('#st-control-3link').css("left", "28%");
+		M('#st-control-3link').css("color", "#FFF");
+		
+		M('#st-control-4').css("left", "42%");
+		M('#st-control-4link').css("left", "42%");
+		M('#st-control-4link').css("color", "#FFF");
+		
+		M('#st-control-5').css("left", "56%");
+		M('#st-control-5link').css("left", "56%");
+		M('#st-control-5link').css("color", "#4FD5D6");
+		
+		M('#st-control-6').css("left", "70%");
+		M('#st-control-6link').css("left", "70%");
+		M('#st-control-6link').css("color", "#FFF");
+		
+		M('#st-control-7').css("left", "85%");
+		M('#st-control-7link').css("left", "85%");
+		M('#st-control-7link').css("color", "#FFF");
 	}
 	
 	function checkPhotography(){ 
@@ -61,6 +200,34 @@
 		K('#st-panel-1').attr('checked',true);
 		var L = jQuery.noConflict();
 		L('.st-scroll').css({"-webkit-transform": "translateY(-500%)"});
+		
+		var M = jQuery.noConflict();
+		M('#st-control-1').css("left", "0%");
+		M('#st-control-1link').css("left", "0%");
+		
+		M('#st-control-2').css("left", "14%");
+		M('#st-control-2link').css("left", "14%");
+		M('#st-control-2link').css("color", "#FFF");
+	
+		M('#st-control-3').css("left", "28%");
+		M('#st-control-3link').css("left", "28%");
+		M('#st-control-3link').css("color", "#FFF");
+		
+		M('#st-control-4').css("left", "42%");
+		M('#st-control-4link').css("left", "42%");
+		M('#st-control-4link').css("color", "#FFF");
+		
+		M('#st-control-5').css("left", "56%");
+		M('#st-control-5link').css("left", "56%");
+		M('#st-control-5link').css("color", "#FFF");
+		
+		M('#st-control-6').css("left", "70%");
+		M('#st-control-6link').css("left", "70%");
+		M('#st-control-6link').css("color", "#4FD5D6");
+		
+		M('#st-control-7').css("left", "85%");
+		M('#st-control-7link').css("left", "85%");
+		M('#st-control-7link').css("color", "#FFF");
 	}
 	
 	function checkFood(){ 
@@ -68,4 +235,32 @@
 		K('#st-panel-1').attr('checked',true);
 		var L = jQuery.noConflict();
 		L('.st-scroll').css({"-webkit-transform": "translateY(-600%)"});
+		
+		var M = jQuery.noConflict();
+		M('#st-control-1').css("left", "0%");
+		M('#st-control-1link').css("left", "0%");
+		
+		M('#st-control-2').css("left", "14%");
+		M('#st-control-2link').css("left", "14%");
+		M('#st-control-2link').css("color", "#FFF");
+	
+		M('#st-control-3').css("left", "28%");
+		M('#st-control-3link').css("left", "28%");
+		M('#st-control-3link').css("color", "#FFF");
+		
+		M('#st-control-4').css("left", "42%");
+		M('#st-control-4link').css("left", "42%");
+		M('#st-control-4link').css("color", "#FFF");
+		
+		M('#st-control-5').css("left", "56%");
+		M('#st-control-5link').css("left", "56%");
+		M('#st-control-5link').css("color", "#FFF");
+		
+		M('#st-control-6').css("left", "70%");
+		M('#st-control-6link').css("left", "70%");
+		M('#st-control-6link').css("color", "#FFF");
+		
+		M('#st-control-7').css("left", "85%");
+		M('#st-control-7link').css("left", "85%");
+		M('#st-control-7link').css("color", "#4FD5D6");
 	}
