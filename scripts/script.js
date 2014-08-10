2eaa3ec055fec77172e8dcfc8a641309d757e541
Asaph Yuan <asaphy@bu.edu>
2014-08-09 17:28:36 -0700

    photography page done

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
 		var K = jQuery.noConflict();
+		
+		var tn1 = K('.mygallery').tn3({
+		skinDir:"skins",
+		imageClick:"fullscreen",
+		image:{
+		maxZoom:1.5,
+		crop:true,
+		clickEvent:"dblclick",
+		transitions:[{
+		type:"blinds"
+		},{
+		type:"grid"
+		},{
+		type:"grid",
+		duration:460,
+		easing:"easeInQuad",
+		gridX:1,
+		gridY:8,
+		// flat, diagonal, circle, random
+		sort:"random",
+		sortReverse:false,
+		diagonalStart:"bl",
+		// fade, scale
+		method:"scale",
+		partDuration:360,
+		partEasing:"easeOutSine",
+		partDirection:"left"
+		}]
+		}
+		});
 
 		K('#slidorion').slidorion({
 			autoPlay: false
